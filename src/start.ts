5836a865953003038abea24fef6f744349926825
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-08-17 18:14:03 -0400

    chore: add simple error logging for migrations and csrf middleware for server fns

--- src/start.ts
+++ src/start.ts
@@ -1,9 +1,13 @@
 import { clerkMiddleware } from '@clerk/tanstack-react-start/server';
-import { createStart } from '@tanstack/react-start';
+import { createCsrfMiddleware, createStart } from '@tanstack/react-start';
+
+const csrfMiddleware = createCsrfMiddleware({
+  filter: (ctx) => ctx.handlerType === 'serverFn',
+});
 
 export const startInstance = createStart(() => {
   return {
-    requestMiddleware: [clerkMiddleware()],
+    requestMiddleware: [csrfMiddleware, clerkMiddleware()],
   };
 });
 
